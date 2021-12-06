807c6448b73dff89579fe2d794bfbba07496bf81
Judson Paiva <judson.paiva@hotmail.com>
2021-12-06 09:07:57 +0100

    Classes Wrappers

---
+++ Garbage-Collector/Wrappers/Wrappers.java
@@ -0,0 +1,18 @@
+package Wrappers;
+
+public class Wrappers {
+    public static void main(String[] args) {
+        
+        // Tipos primitivos
+        int idade = 31;
+        double preco = 12.45;
+        char sexo = 'M';
+        boolean casado = true;
+
+        // classes Wrappers
+
+        Integer wIdade = new Integer(31);
+
+
+    }
+}
